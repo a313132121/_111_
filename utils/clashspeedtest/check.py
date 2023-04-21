06685d4c14a76b80d3a0ad9504f1243f63cb0217
rxsweet <105535100+rxsweet@users.noreply.github.com>
2023-04-21 15:45:07 +0800

    Update check.py

--- utils/clashspeedtest/check.py
+++ utils/clashspeedtest/check.py
@@ -27,13 +27,13 @@ def check(alive, proxy, apiurl, sema, timeout, testurl):
         print(response)
         if response['delay'] > 0:
             alive.append(proxy)
-            """
+            
             url =apiurl + '/proxies/' + str(proxy['name']) + '/delay?url='+testurl2+'&timeout=' + str(timeout)
             r = requests.get(url, timeout=10)
             response = json.loads(r.text)
             if response['delay'] > 0:
                 alive.append(proxy)
-            """
+            
     except:
         pass
     sema.release()
