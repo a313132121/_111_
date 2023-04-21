68a41e195b5d340a9ffe3b743e4fe97b43682d17
rxsweet <105535100+rxsweet@users.noreply.github.com>
2023-04-21 17:05:09 +0800

    Update test_clash_latency.py

--- test_clash_latency.py
+++ test_clash_latency.py
@@ -80,5 +80,9 @@ def test_all_latency(   #latency：潜伏
 
 if __name__ == '__main__':
     #for item in test_all_latency('https://raw.githubusercontent.com/zsokami/sub/main/trials_providers/All.yaml', timeout=10000):
-    for item in test_all_latency('https://raw.githubusercontent.com/rxsweet/proxies/main/sub/sources/staticAll.yaml', timeout=10000):
-        print(*item)    #*参数，**参数 https://zhuanlan.zhihu.com/p/89304906  https://blog.csdn.net/cadi2011/article/details/84871401
+    alive = item in test_all_latency('https://raw.githubusercontent.com/rxsweet/proxies/main/sub/sources/staticAll.yaml', timeout=10000):
+    f = open('xxx.yaml', 'w',encoding="UTF-8")
+    f.write(alive)
+    f.close()
+    #for item in test_all_latency('https://raw.githubusercontent.com/rxsweet/proxies/main/sub/sources/staticAll.yaml', timeout=10000):
+        #print(*item)    #*参数，**参数 https://zhuanlan.zhihu.com/p/89304906  https://blog.csdn.net/cadi2011/article/details/84871401
